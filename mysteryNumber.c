cf8fb69025c1c2ca0d8f2b6c914854e0a593e69a
ubourdon <bourdon.ugo@gmail.com>
2011-10-02 20:11:53 +0200

    it is shorter with do-while instrution

--- mysteryNumber.c
+++ mysteryNumber.c
@@ -12,27 +12,25 @@ int main() {
 	const int  MIN = 1;
 	int misteryNumber = (rand() % (MAX - MIN + 1)) + 1;
 	int number = 0;
-	int counter = 1;
+	int counter = 0;
 
 	printf("\n");
 	printf("Bienvenu dans le jeu du nombre mystère ! \n");
 	printf("Un nombre mystère a été choisi aléatoirement par l'ordinateur \n");
 	printf("Votre mission est de le retrouver \n");
-
-	printf("Veuillez rentrer un nombre en 1 et 100 : \n");
-	scanf("%d", &number);
-
-	while(number != misteryNumber) {
-		if(number > misteryNumber) {
-			printf("%d n'est pas le nombre mystère, il est plus petit \n", number);
-		} else if(number < misteryNumber) {
-			printf("%d n'est pas le nombre mystère, il est plus grand \n", number);
-		}
-
-		printf("Veuillez rentrer un nombre entre 1 et 100 : \n");
+		
+	do {
+		printf("Veuillez rentrer unnombre entre 1 et 100 : ");
 		scanf("%d", &number);
+		printf("\n\n");
+
 		counter++;
-	}
+		
+		if(number > misteryNumber)
+			printf("%d n'est pas le nombre mystère, il est plus petit \n", number);
+		else if(number < misteryNumber)
+			printf("%d n'est pas le nombre mystère, il est plus grand \n", number);
+	} while(number != misteryNumber);
 
 	printf("Bravo vous avez trouvé le nombre mystère ! : %d \n", number);
 	printf("Il vous à fallu %d coups pour trouver le résultat \n", counter);
